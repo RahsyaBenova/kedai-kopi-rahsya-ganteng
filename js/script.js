e28706446da529e23ecd7ece75f2a683497ea0f1
rahsya benova akbar <rahsyabenova@gmail.com>
2023-09-05 09:33:44 +0700

    update webpagee

--- js/script.js
+++ js/script.js
@@ -16,10 +16,19 @@ document.querySelector('#search-button').onclick = (e) => {
     
 }
 
+// Toggle kelas aktif untuk shopping-cart
+const shoppingCart = document.querySelector('.shopping-cart');
+const shoppingButton = document.querySelector('#shopping-cart-button');
+document.querySelector('#shopping-cart-button').onclick = (e) => {
+    shoppingCart.classList.toggle('active');
+    e.preventDefault();
+    
+}
 
 // klik diluar elemen untuk menghilangkan sidebar
 const hm =document.querySelector('#hamburger-menu');
 const sb =document.querySelector('#search-button');
+const sc =document.querySelector('#shopping-cart-button');
 document.addEventListener('click',function(e){
     if(!hm.contains(e.target) && !navbarNav.contains(e.target)){
         navbarNav.classList.remove('active');
@@ -27,4 +36,39 @@ document.addEventListener('click',function(e){
     if(!sb.contains(e.target) && !searchForm.contains(e.target)){
         searchForm.classList.remove('active');
     }
+    if(!sc.contains(e.target) && !shoppingButton.contains(e.target)){
+        shoppingCart.classList.remove('active');
+    }
 });
+
+
+// modal box
+const itemDetailModal = document.querySelector('#item-detail-modal');
+const itemDetailButton = document.querySelectorAll('.item-detail-button');
+
+
+itemDetailButton.forEach((btn) => {
+    btn.onclick = (e) => {
+        itemDetailModal.style.display ='flex';
+        e.preventDefault();
+    };
+});
+   
+
+
+
+
+// close item
+document.querySelector('.modal .close-icon').onclick = (e) =>{
+    itemDetailModal.style.display = 'none';
+    e.preventDefault();
+};
+
+
+// klik dluar item
+
+window.onclick = (e) => {
+    if (e.target === itemDetailModal){
+        itemDetailModal.style.display = 'none';
+    }
+}
